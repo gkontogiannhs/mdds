615644a7991de5b031b5a56f7376a9ef843b8ea2
George Kontogiannis <100489926+gkontogiannhs@users.noreply.github.com>
2022-12-31 11:37:38 +0200

    second commit

--- main.py
+++ main.py
@@ -12,17 +12,15 @@ class RangeTree:
         self.root = self.build_tree(self.points)
 
     def build_tree(self, points):
-        # Sort the points by the secdary field
-        points.sort(key=lambda x: x[1], reverse=True)
+        # Sort the points by the surname and awards fields
+        points.sort(key=lambda x: (x[0], x[1]))
 
         # Find the median point
         median = len(points) // 2
 
-        # Create a new node with the median point
+        # Create a new node with the median surname and median number of awards
         node = Node(points[median][0], points)
-        # print(f'-> {node.value}: {node.points}')
-        # print(f'And left will get: {points[:median]}')
-        # print(f'And right will get: {points[median + 1:]}')
+
         # Recursively build the left and right subtrees
         node.left = self.build_tree(points[:median]) if points[:median] else None
         node.right = self.build_tree(points[median + 1:]) if points[median + 1:] else None
@@ -35,31 +33,29 @@ class RangeTree:
         if root is None or not root.points:
             return []
 
-        # Check the points in the root node
+        # Create a list to store the returned points
+        returned_points = []
+        # Create a list to store the points to be returned
         points = []
+
+        # Check the points in the root node
         for point in root.points:
             surname, num_awards, _ = point
             # If the point has the correct number of awards, and the surname is within the search range, add it to the results list
-            if surname_range[0] <= surname[0] <= surname_range[1] and num_awards >= awards:
-                    points.append(point)
+            if surname_range[0] <= surname[0] <= surname_range[1] and num_awards >= awards and point not in returned_points:
+                points.append(point)
+                returned_points.append(point)
 
         # Recursively search the left and right subtrees
         if surname_range[0] <= root.value:
             points.extend(self.search(root.left, awards, surname_range))
-        if surname_range[1] >= root.value:
+        if surname_range[1] > root.value:
             points.extend(self.search(root.right, awards, surname_range))
 
         return points
 
 
-    def print_tree(self, root, level=0):
-        if root is not None:
-            self.print_tree(root.left, level + 1)
-            print(' ' * 4 * level + '->', root.value, root.points)
-            self.print_tree(root.right, level + 1)
-
-
-# Create a list of documents
+# Create a list of points to be inserted into the RangeTree
 documents = [
     ('Brown', 1, [1, 1, 0, 0]),
     ('Johnson', 3, [1, 0, 1, 0]),
@@ -68,12 +64,6 @@ documents = [
     ('Williams', 2, [0, 1, 0, 1])
 ]
 
-# Create a range tree from the documents
 tree = RangeTree(documents)
-
-# Print the tree
-tree.print_tree(tree.root)
-
-# Search the tree
 results = tree.search(tree.root, 2, ('A', 'J'))
 print(results)
