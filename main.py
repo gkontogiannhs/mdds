6cfab591529f497822e0faf12b1765aeaa68acca
George Kontogiannis <100489926+gkontogiannhs@users.noreply.github.com>
2023-01-01 18:13:24 +0200

    correct impl on 1D, 2D returns wrong x

--- main.py
+++ main.py
@@ -1,69 +1,159 @@
 class Node:
-    def __init__(self, value, points):
+    def __init__(self, left=None, right=None, value=None, y_tree=None):
+        self.left = left
+        self.right = right
         self.value = value
-        self.points = points
-        self.left = None
-        self.right = None
+        self.y_tree = y_tree
 
 
-class RangeTree:
-    def __init__(self, points):
-        self.points = points
-        self.root = self.build_tree(self.points)
+class RangeTree1D:
+    def __init__(self, points, axis=0):
+        self.axis = axis
+        self.root = self._build_tree(points)
+    
 
-    def build_tree(self, points):
-        # Sort the points by the surname and awards fields
-        points.sort(key=lambda x: (x[0], x[1]))
-
-        # Find the median point
+    def _build_tree(self, points):
+        if not points:
+            return None
+        
+        if len(points) == 1:
+            return Node(value=points[0])
+        
+        points.sort(key=lambda point: point[self.axis])
         median = len(points) // 2
+        left = self._build_tree(points[:median])
+        right = self._build_tree(points[median+1:])
+        
+        return Node(left=left, right=right, value=points[median])
+    
 
-        # Create a new node with the median surname and median number of awards
-        node = Node(points[median][0], points)
-
-        # Recursively build the left and right subtrees
-        node.left = self.build_tree(points[:median]) if points[:median] else None
-        node.right = self.build_tree(points[median + 1:]) if points[median + 1:] else None
-
-        return node
-
-
-    def search(self, root, awards, surname_range):
-        # If the root is None or has no points, return an empty list
-        if root is None or not root.points:
+    def query(self, range):
+        if not self.root:
             return []
 
-        # Create a list to store the returned points
-        returned_points = []
-        # Create a list to store the points to be returned
-        points = []
+        if range[0] <= self.root.value[self.axis] <= range[1]:
+            return self._get_values(self.root, range)
 
-        # Check the points in the root node
-        for point in root.points:
-            surname, num_awards, _ = point
-            # If the point has the correct number of awards, and the surname is within the search range, add it to the results list
-            if surname_range[0] <= surname[0] <= surname_range[1] and num_awards >= awards and point not in returned_points:
-                points.append(point)
-                returned_points.append(point)
+        values = []
+        if range[0] > self.root.value[self.axis]:
+            if self.root.right:
+                values += self._get_values(self.root.right, range)
+        elif range[1] < self.root.value[self.axis]:
+            if self.root.left:
+                values += self._get_values(self.root.left, range)
+        return values
 
-        # Recursively search the left and right subtrees
-        if surname_range[0] <= root.value:
-            points.extend(self.search(root.left, awards, surname_range))
-        if surname_range[1] > root.value:
-            points.extend(self.search(root.right, awards, surname_range))
+        
 
-        return points
+    def _get_values(self, node, range):
+        if not node:
+            return []
+        
+        values = []
+        if range[0] <= node.value[self.axis] <= range[1]:
+            values.append(node.value)
+        
+        if node.left:
+            values += self._get_values(node.left, range)
+        if node.right:
+            values += self._get_values(node.right, range)
+        
+        return values
+    
+
+    def print_tree(self):
+        self._print_tree(self.root, 0)
+        
+    def _print_tree(self, root, depth):
+        if not root:
+            return
+        self._print_tree(root.right, depth + 1)
+        print("  " * depth + "-> " + str(root.value))
+        self._print_tree(root.left, depth + 1)
 
 
-# Create a list of points to be inserted into the RangeTree
-documents = [
-    ('Brown', 1, [1, 1, 0, 0]),
-    ('Johnson', 3, [1, 0, 1, 0]),
-    ('Jones', 3, [0, 1, 1, 0]),
-    ('Smith', 2, [1, 0, 0, 1]),
-    ('Williams', 2, [0, 1, 0, 1])
-]
+class RangeTree2D:
+    def __init__(self, points, axis=0):
+        self.axis = axis
+        self.root = self._build_tree(points)
 
-tree = RangeTree(documents)
-results = tree.search(tree.root, 2, ('A', 'J'))
-print(results)
+    def _build_tree(self, points):
+        if not points:
+            return None
+
+        if len(points) == 1:
+            return Node(value=points[0], y_tree=RangeTree1D(points, axis=1))
+
+        # sort by x coord
+        points.sort(key=lambda point: point[self.axis])
+        
+        median = len(points) // 2
+
+        left_points = points[:median]
+        right_points = points[median+1:]
+
+        left = RangeTree2D(left_points) if left_points else None
+        right = RangeTree2D(right_points) if right_points else None
+
+        return Node(left=left, right=right, value=points[median], y_tree=RangeTree1D(points, axis=1))
+
+
+
+    def query(self, x_range, y_range):
+        if self.root is None: return []
+        
+        if x_range[0] <= self.root.value[self.axis] <= x_range[1]:
+            # Return only the points in the y_tree that fall within the y-range of the query
+            return [point for point in self.root.y_tree.query(y_range) if x_range[0] <= point[0] <= x_range[1]]
+        
+        values = []
+        if x_range[0] > self.root.value[self.axis]:
+            if self.root.right:
+                values += self.root.right.query(x_range, y_range)
+        elif x_range[1] < self.root.value[self.axis]:
+            if self.root.left:
+                values += self.root.left.query(x_range, y_range)
+        return values
+
+
+
+    def print_tree(self):
+        self._print_tree(self.root, 0)
+
+    def _print_tree(self, root, depth):
+        if not root:
+            return
+        self._print_tree(root.right, depth + 1)
+        print("  " * 2 * depth + "-> " + str(root.value))
+        self._print_tree(root.left, depth + 1)
+        print("  " * 2 * depth + "    |")
+        print("  " * depth + "    +-- Y Tree:")
+        root.y_tree.print_tree()
+
+
+
+from random import randint
+# points = [(1, 4), (3, 4), (12, 5), (5, 6), (7, 8), (9, 10), (11, 3), (2, 9), (4, 5), (5, 7), (6, 8), (8, 2), (9, 4), (10, 5), (11, 6), (1, 2), (3, 5), (4, 6), (5, 7), (6, 8)]
+
+points = [(randint(0, 100), randint(0, 100)) for _ in range(50)]
+print(points)
+print()
+temp  = []
+for point in points:
+    if 30 <= point[0] <= 60 and 20 <= point[1] <= 50:
+        temp += [point]
+
+temp.sort()
+print(temp)
+print()
+
+# create a 2D range tree with x as the main axis
+tree = RangeTree2D(points, axis=0)
+
+# query the tree for all points with x in the range [4, 12] and y in the range [5, 8]
+result = tree.query((30, 60), (20, 50))
+result.sort()
+print(result)  # should print [(5, 6), (7, 8)]
+
+
+
